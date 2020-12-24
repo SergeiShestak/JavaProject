a546ffde61752df9506f5ff597ee3ff1be02da02
Sergei Shestak <sshestak1970@gmail.com>
2020-12-24 14:48:14 -0500

    without .gitignore

---
+++ src/test/java/MultiClubMemberTest.java
@@ -0,0 +1,9 @@
+import javaproject.MultiClubMember;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MultiClubMemberTest {
+
+}
